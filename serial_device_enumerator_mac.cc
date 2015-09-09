7e36e0461aa49093cbc9a096a638f53b28391cb2
Charlie Andrews <charliea@google.com>
2015-09-09 16:58:30 -0400

    Changed to use IORegistryEntrySearchCFProperty

--- serial_device_enumerator_mac.cc
+++ serial_device_enumerator_mac.cc
@@ -1,92 +1,87 @@
 #include <stdio.h>
+#include <string>
 
 #include <CoreFoundation/CFNumber.h>
-#include <IOKit/IOKitLib.h>
 #include <IOKit/serial/IOSerialKeys.h>
-#include <IOKit/storage/IOMedia.h>
 #include <IOKit/usb/IOUSBLib.h>
-#include <IOKit/usb/USBSpec.h>
+
+using std::string;
+
+template<typename T> T GetRegistryProperty(
+    io_service_t device, CFStringRef keyName) {
+  // Make sure that we search ancestor entries as well. The registry entry that
+  // contains the USB properties is higher up the registry tree than the entry
+  // that contains the serial properties.
+  return (T) IORegistryEntrySearchCFProperty(
+      device, kIOServicePlane, keyName, NULL,
+      kIORegistryIterateRecursively | kIORegistryIterateParents);
+}
+
+bool GetStringProperty(
+    io_service_t device, CFStringRef keyName, string* returnValue) {
+  CFStringRef propertyValue = GetRegistryProperty<CFStringRef>(device, keyName);
+  if (propertyValue) {
+    CFStringEncoding encodingMethod = CFStringGetSystemEncoding();
+    const char* str = CFStringGetCStringPtr(propertyValue, encodingMethod);
+
+    if (str) {
+      *returnValue = string(str);
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool GetIntProperty(
+    io_service_t device, CFStringRef keyName, int* returnValue) {
+  CFNumberRef propertyValue = GetRegistryProperty<CFNumberRef>(device, keyName);
+  if (propertyValue)
+    return CFNumberGetValue(propertyValue, kCFNumberIntType, returnValue);
+
+  return false;
+}
 
 int main() {
   // Find and iterate through all serial devices.
   CFMutableDictionaryRef matchingDict;
   matchingDict = IOServiceMatching(kIOSerialBSDServiceValue);
   if (matchingDict == NULL)
-    return -1;
+    return 1;
 
   io_iterator_t iter;
-  if (IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDict, &iter) != KERN_SUCCESS)
-    return -1;
+  if (IOServiceGetMatchingServices(
+          kIOMasterPortDefault, matchingDict, &iter) != KERN_SUCCESS)
+    return 1;
 
   io_service_t device;
   while ((device = IOIteratorNext(iter))) {
-    CFMutableDictionaryRef registryProperties;
-    CFNumberRef cfNumber = NULL;
-    CFStringRef cfString = NULL;
-    CFStringEncoding encodingMethod = CFStringGetSystemEncoding();
-
-    const char* dialinDevice = NULL;
-    const char* calloutDevice = NULL;
-    const char* productString = NULL;
-    short vendorId = -1;
-    short productId = -1;
-
-    // Get the callout and dialin paths for the device.
-    if (IORegistryEntryCreateCFProperties(device, &registryProperties, NULL, kNilOptions) != KERN_SUCCESS) {
-      IOObjectRelease(device);
-      continue;
+    string calloutDevice;
+    if (GetStringProperty(device, CFSTR(kIOCalloutDeviceKey), &calloutDevice)) {
+      printf("Callout device: %s\n", calloutDevice.c_str());
     }
 
-    if (CFDictionaryGetValueIfPresent(registryProperties, CFSTR(kIOCalloutDeviceKey), (const void **) &cfString)) {
-      calloutDevice = CFStringGetCStringPtr(cfString, encodingMethod);
+    string dialinDevice;
+    if (GetStringProperty(device, CFSTR(kIODialinDeviceKey), &dialinDevice)) {
+      printf("Dialin device: %s\n", dialinDevice.c_str());
     }
 
-    if (CFDictionaryGetValueIfPresent(registryProperties, CFSTR(kIODialinDeviceKey), (const void **) &cfString)) {
-      dialinDevice = CFStringGetCStringPtr(cfString, encodingMethod);
+    string productString;
+    if (GetStringProperty(device, CFSTR(kUSBProductString), &productString)) {
+      printf("Product string: %s\n", productString.c_str());
     }
 
-    // On Mac, devices are organized into a tree structure. For an
-    // IOSerialBSDClient that is also an IOUSBDevice, we traverse the
-    // registry tree upwards from the IOSerialBSDClient entry until we
-    // hit the IOUSBDevice entry. Once there, we can access the
-    // device properties that we need.
-    io_registry_entry_t curr;
-    kern_return_t kr = IORegistryEntryGetParentEntry(device, kIOServicePlane, &curr);
-    while (kr == KERN_SUCCESS) {
-      if (IOObjectConformsTo(curr, kIOUSBDeviceClassName)) {
-        if (IORegistryEntryCreateCFProperties(curr, &registryProperties, NULL, kNilOptions) != KERN_SUCCESS) {
-          break;
-        }
-
-        if (CFDictionaryGetValueIfPresent(registryProperties, CFSTR(kUSBProductString), (const void **) &cfString)) {
-          productString = CFStringGetCStringPtr(cfString, encodingMethod);
-        }
-
-        if (CFDictionaryGetValueIfPresent(registryProperties, CFSTR(kUSBVendorID), (const void **) &cfNumber)) {
-          CFNumberGetValue(cfNumber, kCFNumberIntType, &vendorId);
-        }
-
-        if (CFDictionaryGetValueIfPresent(registryProperties, CFSTR(kUSBProductID), (const void **) &cfNumber)) {
-          CFNumberGetValue(cfNumber, kCFNumberIntType, &productId);
-        }
-
-        break;
-      }
-
-      io_registry_entry_t next;
-      kr = IORegistryEntryGetParentEntry(curr, kIOServicePlane, &next);
-      IOObjectRelease(curr);
-      curr = next;
+    int vendorId;
+    if (GetIntProperty(device, CFSTR(kUSBVendorID), &vendorId)) {
+      printf("Vendor ID: 0x%04x\n", vendorId);
     }
 
-    IOObjectRelease(curr);
+    int productId;
+    if (GetIntProperty(device, CFSTR(kUSBProductID), &productId)) {
+      printf("Product ID: 0x%04x\n", productId);
+    }
+
     IOObjectRelease(device);
-
-    printf("Dialin device: %s\n", dialinDevice);
-    printf("Callout device: %s\n", calloutDevice);
-    printf("Product string: %s\n", productString);
-    printf("Vendor ID: 0x%04x\n", vendorId);
-    printf("Product ID: 0x%04x\n", productId);
     printf("\n");
   }
 
